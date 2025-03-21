074dc0a3ee1b3d9a9fa87626ee4ed2b3333dbfaf
Brandon Kim <105524304+Sir-Encoded@users.noreply.github.com>
2025-03-21 16:48:23 -0600

    MCL perchance

--- src/auton/skills.cpp
+++ src/auton/skills.cpp
@@ -83,7 +83,8 @@ pros::delay(1100);
 chassis.moveToPoint(25, 39, 900);
 pros::delay(200);
 chassis.moveToPoint(34, 42, 800);
-chassis.moveToPoint(52, 78, 2000);
+chassis.moveToPoint(52, 81, 2000);
+pros::delay(200);
 pros::Task([]{
        pros::delay(650);
        liftControl(wallstakeStates[1]);
@@ -124,6 +125,10 @@ chassis.moveToPoint(-5, 115, 1000, {.forwards = false, .maxSpeed = 65});
 pros::Task([]{
        pros::delay(600);
        clampPiston.set_value(true);
+       pros::delay(200);
+       leftDoinkerPiston.set_value(true);
+       pros::delay(200);
+       leftDoinkerPiston.set_value(false);
        pros::delay(200);
        intake.move_voltage(12000);
 });
